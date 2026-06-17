8f8b6239deec5aecc4b2e23082d6af065cce55a1
Nurlan <suleymanlinurlan85@gmail.com>
2026-06-17 20:00:24 +0400

    added dependencies and setup project dependencies

---
+++ src/main/java/com/nurlansuleymanli/metagraphapi/MetaGraphApiApplication.java
@@ -0,0 +1,13 @@
+package com.nurlansuleymanli.metagraphapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaGraphApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaGraphApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nurlansuleymanli/metagraphapi/MetaGraphApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurlansuleymanli.metagraphapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaGraphApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
