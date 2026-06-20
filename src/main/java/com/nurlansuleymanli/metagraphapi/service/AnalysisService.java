23108c783d35a8d78f202c5416a031d5e45e5f0d
Nurlan <suleymanlinurlan85@gmail.com>
2026-06-20 20:51:35 +0400

    added analysis service and write analyze logic

---
+++ src/main/java/com/nurlansuleymanli/metagraphapi/service/AnalysisService.java
@@ -0,0 +1,52 @@
+package com.nurlansuleymanli.metagraphapi.service;
+
+import com.nurlansuleymanli.metagraphapi.dto.AnalysisDto;
+import com.nurlansuleymanli.metagraphapi.dto.PostDto;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE,makeFinal = true)
+public class AnalysisService {
+
+    public AnalysisDto analyzePosts(List<PostDto> posts){
+
+        posts.forEach(post ->
+                post.setEngagement(
+                        post.getLikeCount() + post.getCommentsCount()
+                )
+        );
+
+        List<PostDto> topPosts = posts.stream()
+                .sorted(Comparator.comparing(PostDto::getEngagement).reversed())
+                .limit(3)
+                .toList();
+
+
+        Map<String, Integer> likesByDay = posts.stream()
+                .collect(Collectors.groupingBy(postDto -> {
+                            LocalDateTime date = LocalDateTime.parse(postDto.getTimestamp());
+                            return date.getDayOfWeek().toString();
+                        },
+                        Collectors.summingInt(PostDto::getLikeCount)
+                        ));
+
+        return AnalysisDto.builder()
+                .likesByDay(likesByDay)
+                .topEngagementPosts(topPosts)
+                .summary("The last 20 posts were analyzed and the 3 posts that received the most likes" +
+                        " and comments and the number of likes of the posts by day of the week were presented to you." +
+                        "These analyses were conducted based on the number of comments and likes.")
+                .build();
+
+    }
+
+
+}
