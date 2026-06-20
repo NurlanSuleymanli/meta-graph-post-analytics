9c95cfc021cb28b860f2062f060c1317d034ac56
Nurlan <suleymanlinurlan85@gmail.com>
2026-06-20 20:01:04 +0400

    added metaGraph client class with feign client

---
+++ src/main/java/com/nurlansuleymanli/metagraphapi/client/MetaGraphClient.java
@@ -0,0 +1,18 @@
+package com.nurlansuleymanli.metagraphapi.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@FeignClient(name = "Meta-Graph", url = "https://graph.facebook.com/v25.0")
+public interface MetaGraphClient {
+
+    @GetMapping("/{userId}/media")
+    public ResponseEntity<?> getPosts(@PathVariable Long userId,
+                                      @RequestParam String field,
+                                      @RequestParam int limit,
+                                      @RequestParam("access_token") String token);
+}
