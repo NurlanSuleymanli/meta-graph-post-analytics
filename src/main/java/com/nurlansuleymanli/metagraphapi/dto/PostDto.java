6ac1afff9a01d6180f24ea80c8bebe794f9fa32a
Nurlan <suleymanlinurlan85@gmail.com>
2026-06-21 15:03:55 +0400

    add annotation for 2 parameters for json data parse problem

--- src/main/java/com/nurlansuleymanli/metagraphapi/dto/PostDto.java
+++ src/main/java/com/nurlansuleymanli/metagraphapi/dto/PostDto.java
@@ -1,5 +1,6 @@
 package com.nurlansuleymanli.metagraphapi.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
@@ -11,7 +12,9 @@ public class PostDto {
     String id;
     String caption;
     String timestamp;
+    @JsonProperty("like_count")
     int likeCount;
+    @JsonProperty("comments_count")
     int commentsCount;
     int engagement;
 
